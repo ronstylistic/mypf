a14e3095751c6b3c282c89ddce2e8671b0eeb5f8
ronstylistic <ronnelbedana2011@gmail.com>
2026-02-11 02:43:51 +0800

    prisma fix

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import "dotenv/config"
+import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
   datasource: {
     url: process.env.MYPF_DATABASE_URL
-  },
-  migrations: {
-    path: 'prisma/migrations'
-  },
+  }
 })
